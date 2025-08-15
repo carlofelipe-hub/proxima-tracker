2f51c9574f65a754c7e287dec763777ce0dff158
carlofelipe-hub <carlofelipe101@gmail.com>
2025-08-15 14:11:17 +0800

    fix auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,6 @@
-import NextAuth from "@/auth"
+import NextAuth from "next-auth"
+import { authOptions } from "@/auth"
 
-export const { GET, POST } = NextAuth
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
